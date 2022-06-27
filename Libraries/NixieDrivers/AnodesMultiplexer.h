da8afcc4b0bceae5e0c2fbc251c8a1b6bb8f9ac4
Szymon Grzegorzewski <szymon.s.grzegorzewski@gmail.com>
2022-06-27 18:44:44 +0200

    add preprocessor directives to turn on and off nixie tubes

--- Libraries/NixieDrivers/AnodesMultiplexer.h
+++ Libraries/NixieDrivers/AnodesMultiplexer.h
@@ -9,17 +9,20 @@
 #define LIBRARIES_NIXIEDRIVERS_ANODESMULTIPLEXER_H_
 
 
-#define ANODES_INIT DDRD |= 0b00000011; DDRB |= 0b10000001
-
+#define ANODES_INIT DDRD |= 0b11000000; DDRB |= 0b10000001
+//tens of hours
 #define NIXIE_3_ON   PORTB |=  (1<<PB0)
 #define NIXIE_3_OFF  PORTB &=~ (1<<PB0)
 
+//units of hours
 #define NIXIE_2_ON   PORTD |=  (1<<PD7)
 #define NIXIE_2_OFF  PORTD &=~ (1<<PD7)
 
+//tens of minutes
 #define NIXIE_1_ON   PORTB |=  (1<<PB7)
 #define NIXIE_1_OFF  PORTB &=~ (1<<PB7)
 
+//units of minutes
 #define NIXIE_0_ON   PORTD |=  (1<<PD6)
 #define NIXIE_0_OFF  PORTD &=~ (1<<PD6)
 
