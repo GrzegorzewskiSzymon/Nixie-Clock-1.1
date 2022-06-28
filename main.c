1293fc80ac6dc9258e1ec122556b21e2bef33ea2
Szymon Grzegorzewski <szymon.s.grzegorzewski@gmail.com>
2022-06-29 01:20:15 +0200

    using previously written funcions for: -basic initialization of periferies -simple pulling data from rtc with displaying it

--- main.c
+++ main.c
@@ -5,9 +5,14 @@
  *      Author: Grzegorzewski Szymon
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
-#include "Libraries/Utils/utils.h"
 
+#include "Libraries/Utils/utils.h"
+#include "Libraries/NixieDrivers/AnodesMultiplexer.h"
+#include "Libraries/NixieDrivers/SN74141.h"
+#include "Libraries/RTC/i2c.h"
+#include "Libraries/RTC/pcf8583.h"
 
 int main(void)
 {
@@ -16,9 +21,19 @@ int main(void)
 	Led_Init();
 	BlinkLed(); //visual confirmation of reset
 
+	SwitchInit(&Switch_0, &Switch_1, &Switch_2);//Numerate and pull up switches
 
+	Anodes_Init();//Set pins for transoptor as output
+	BCD_Init();
+
+	//Put actual hour to rtc
+	RtcInit();
+
+	uint8_t seconds, minutes, hours;
 	while(1)
 	{
+		RtcReadData(&seconds, &minutes, &hours);
+		DisplayTime(hours, minutes);
 
 	}
 
