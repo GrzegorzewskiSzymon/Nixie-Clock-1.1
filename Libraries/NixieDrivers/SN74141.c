52e9d92da27c3d45f49207b9544cbbb7c1952dda
Szymon Grzegorzewski <szymon.s.grzegorzewski@gmail.com>
2022-07-04 01:31:11 +0200

    add one millisecond of turned off tubes between switching between them, because optocoupler LTV-352T switch off characteristics coused ghost digits

--- Libraries/NixieDrivers/SN74141.c
+++ Libraries/NixieDrivers/SN74141.c
@@ -22,53 +22,67 @@ void DisplayTime(int8_t hours, int8_t minutes)
 
 	if(millis-millis_DisplayTime>20)//Its time to display units of minutes
 	{
-		PORTD &=~ 0b00001111;
-		PORTD |= minutes%10;
+		NIXIE_3_OFF;NIXIE_2_OFF;NIXIE_1_OFF;NIXIE_0_OFF;
+		if(millis-millis_DisplayTime>21)//one ms later
+		{
+			PORTD &=~ 0b00001111;
+			PORTD |= minutes%10;
 
-		NIXIE_3_OFF;
-		NIXIE_2_OFF;
-		NIXIE_1_OFF;
-		NIXIE_0_ON;
+			NIXIE_3_OFF;
+			NIXIE_2_OFF;
+			NIXIE_1_OFF;
+			NIXIE_0_ON;
 
-		millis_DisplayTime = millis;//set actual time after full cicle
+			millis_DisplayTime = millis;//set actual time after full cicle
+		}
 		return;
 	}
 
 	if(millis-millis_DisplayTime>15)//Its time to display tens of minutes
 	{
-		PORTD &=~ 0b00001111;
-		PORTD |= minutes/10;
+		NIXIE_3_OFF;NIXIE_2_OFF;NIXIE_1_OFF;NIXIE_0_OFF;
+		if(millis-millis_DisplayTime>16)//one ms later
+		{
+			PORTD &=~ 0b00001111;
+			PORTD |= minutes/10;
 
-		NIXIE_3_OFF;
-		NIXIE_2_OFF;
-		NIXIE_1_ON;
-		NIXIE_0_OFF;
-
+			NIXIE_3_OFF;
+			NIXIE_2_OFF;
+			NIXIE_1_ON;
+			NIXIE_0_OFF;
+		}
 		return;
 	}
 
 	if(millis-millis_DisplayTime>10)//Its time to display units of hours
 	{
-		PORTD &=~ 0b00001111;
-		PORTD |= hours%10;
-		NIXIE_3_OFF;
-		NIXIE_2_ON;
-		NIXIE_1_OFF;
-		NIXIE_0_OFF;
+		NIXIE_3_OFF;NIXIE_2_OFF;NIXIE_1_OFF;NIXIE_0_OFF;
+		if(millis-millis_DisplayTime>11)//one ms later
+		{
+			PORTD &=~ 0b00001111;
+			PORTD |= hours%10;
 
+			NIXIE_3_OFF;
+			NIXIE_2_ON;
+			NIXIE_1_OFF;
+			NIXIE_0_OFF;
+		}
 		return;
 	}
 
 	if(millis-millis_DisplayTime>5 && hours>9)//Its time to display tens of hours
 	{
-		PORTD &=~ 0b00001111;
-		PORTD |= hours/10;
-		NIXIE_3_ON;
-		NIXIE_2_OFF;
-		NIXIE_1_OFF;
-		NIXIE_0_OFF;
+		NIXIE_3_OFF;NIXIE_2_OFF;NIXIE_1_OFF;NIXIE_0_OFF;
+		if(millis-millis_DisplayTime>6)//one ms later
+		{
+			PORTD &=~ 0b00001111;
+			PORTD |= hours/10;
 
-		return;
+			NIXIE_3_ON;
+			NIXIE_2_OFF;
+			NIXIE_1_OFF;
+			NIXIE_0_OFF;
+		}
 	}
 
 }
