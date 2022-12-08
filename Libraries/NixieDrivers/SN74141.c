430e96b7e1823be66b29ade4c6fab06a0e805ba2
GrzegorzewskiSzymon <szymon.s.grzegorzewski@gmail.com>
2022-12-08 23:20:24 +0100

    turn off all tubes for one ms after every switch between displaying tens and units to cancel ghost numbers in tubes

--- Libraries/NixieDrivers/SN74141.c
+++ Libraries/NixieDrivers/SN74141.c
@@ -31,41 +31,49 @@ void DisplayTime(int8_t hours, int8_t minutes, int8_t seconds)
 
 	if(millis-millis_DisplayTime>10)//Its time to display tens
 	{
-		NIXIE_TENS_ON;
+		NIXIE_TENS_OFF;
 		NIXIE_UNITS_OFF;
+		if(millis-millis_DisplayTime>11)//shift one ms to cancel ghost numbers in tubes
+		{
+			NIXIE_TENS_ON;
 
-		//displaying  tens of hours
-		PORTB &=~ 0b00001111;
-		PORTB |= hours/10;
+			//displaying  tens of hours
+			PORTB &=~ 0b00001111;
+			PORTB |= hours/10;
 
-		//displaying  tens of minutes
-		PORTB &=~ 0b11110000;
-		PORTB |= ((minutes/10)<<4);
+			//displaying  tens of minutes
+			PORTB &=~ 0b11110000;
+			PORTB |= ((minutes/10)<<4);
 
-		//displaying  tens of minutes
-		PORTC &=~ 0b00001111;
-		PORTC |= seconds/10;
+			//displaying  tens of minutes
+			PORTC &=~ 0b00001111;
+			PORTC |= seconds/10;
 
-		millis_DisplayTime = millis;//set actual time after full cicle
-		return;
+			millis_DisplayTime = millis;//set actual time after full cicle
+			return;
+		}
 	}
 
 	if(millis-millis_DisplayTime>5)//Its time to display units
 	{
 		NIXIE_TENS_OFF;
-		NIXIE_UNITS_ON;
+		NIXIE_UNITS_OFF;
+		if(millis-millis_DisplayTime>6)//shift one ms to cancel ghost numbers in tubes
+		{
+			NIXIE_UNITS_ON;
 
-		//displaying units of hours
-		PORTB &=~ 0b00001111;
-		PORTB |= hours%10;
+			//displaying units of hours
+			PORTB &=~ 0b00001111;
+			PORTB |= hours%10;
 
-		//displaying units of minutes
-		PORTB &=~ 0b11110000;
-		PORTB |= ((minutes%10)<<4);
+			//displaying units of minutes
+			PORTB &=~ 0b11110000;
+			PORTB |= ((minutes%10)<<4);
 
-		//displaying units of seconds
-		PORTC &=~ 0b00001111;
-		PORTC |= seconds%10;
+			//displaying units of seconds
+			PORTC &=~ 0b00001111;
+			PORTC |= seconds%10;
+		}
 	}
 }
 
